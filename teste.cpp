8013b1233c1c4eeffce3f4851a5e429e885861a0
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2023-12-13 16:38:34 -0300

    had wrong file there :(((

--- teste.cpp
+++ teste.cpp
@@ -1,276 +1,101 @@
-#ifndef NN_H
-#define NN_H
-
-
 #include <stdio.h>
 #include <vector>
-#include <cmath>
+#include <iostream>
 #include <functional>
-using namespace std;
 
+#include "graph.h"
 #include "matrix.h"
 #include "vector.h"
 #include "dataset.h"
+#include "nn.h"
+
+using namespace std;
+
+int main() {
+
+	// create the neural network
+	NN network({ 1, 20, 1 });
 
 
-#define PI 3.1415926535
+	Graph graph(800, 650, 1, 1);
+	Line trainingAccuracy(olc::Pixel(44, 160, 44));
+	Line validationAccuracy(olc::Pixel(214, 39, 40));
+	graph.addLine(&trainingAccuracy);
+	graph.addLine(&validationAccuracy);
+
+	Line actualCos(olc::Pixel(31, 119, 180));
+	Line networkPred(olc::Pixel(255, 127, 14));
+
+	for (double i = 0; i <= 6.2831; i += 0.05) {
+		networkPred.addPoint(Point(i, network.feedForward(Vec({ i / 6.2831 }))[0] * 2 - 1));
+	}
+	graph.addLine(&networkPred);
 
 
+	// create the training data
+	Dataset training;
+	for (double i = 0; i <= 6.2831; i += 0.05) {
+		DataPoint data(
+			Vec({ i }), Vec({ std::sin(i) })
+		);
 
-Vec activation(const Vec& x) {
-	return 1 / (1 + Vec::exp(-x));
-}
+		actualCos.addPoint(Point(i, std::sin(i)));
 
-Vec activationDerivative(const Vec& x) {
-	Vec s = activation(x);
-	return Vec::hadamard(s, 1 - s);
-}
-
-double cost(Vec& pred, Vec& y) {
-	return Vec::sum((pred - y) ^ 2);
-}
-
-Vec costDerivative(Vec& pred, Vec& y) {
-	return pred - y;
-}
-
-
-
-
-
-
-
-template <typename T>
-struct Adam {
-	double a = 0.004;
-	double b1 = 0.9;
-	double b2 = 0.999;
-	double l = 0.01;
-
-	int t = 0;
-
-	double b1PowerT = 1;
-	double b2PowerT = 1;
-
-	T m;
-	T v;
-	T& x;
-
-
-	Adam(T& parameter) : x(parameter), m(T::zeros(parameter.size)), v(T::zeros(parameter.size)) {
-
+		training.add(data);
 	}
 
-	void step(T&& gradient) {
+	// put the training data in the [0, 1] range
+	training.normalize(0, 1);
 
-	/*	x -= gradient * 0.3;
+	graph.addLine(&actualCos);
 
-		return;*/
 
-		++t;
-		b1PowerT *= b1;
-		b2PowerT *= b2;
+	// create the validation data
+	Dataset validation;
+	for (double i = 0; i < 6.2831; i += 0.05) {
 
-	//	gradient += x * l;
+		DataPoint data(
+			Vec({ i }), Vec({ std::sin(i) })
+		);
 
-		m = T::lerp(gradient, m, b1);
-		v = T::lerp(T::hadamard(gradient, gradient), v, b2);
-
-		T m_hat = m / (1 - b1PowerT);
-		T v_hat = v / (1 - b2PowerT);
-
-		T c = (m_hat / (T::sqrt(v_hat) + 1e-8)/* + x * l*/);
-		double n = linearWarmup(t);
-
-		x -= c * a * n;
+		validation.add(data);
 	}
 
-	double linearWarmup(int t) {
-		if (t < 3e4) { // warm-up
-			return 1 * t * 3e-4;
+	// put the validation data in the [0, 1] range
+	validation.normalize(0, 1);
+
+
+
+
+	std::function<bool(int)> updateFunc = [&](int dataSeen) {
+
+		int index = 0;
+		for (double i = 0; i <= 6.2831; i += 0.05) {
+			networkPred.points[index++] = Point(i, network.feedForward(Vec({ i / 6.2831 }))[0] * 2 - 1);
 		}
 
-		return 1 / (1 + 5e-5 * (t - 3e4));
-	}
 
-	double linearWarmupCosAnnealing(int t, int span, int warmup, int hold = 0, double min = 0, double max = 1) {
-		t %= span;
+		double trainingCost = network.estimateCost(training);
+		double validationCost = network.estimateCost(validation);
 
-		if (t < warmup) {
-			return (max - min) / warmup * t + min;
+		validationAccuracy.addPoint(Point((double) dataSeen * 1e-5, validationCost * 120));
+		trainingAccuracy.addPoint(Point((double) dataSeen * 1e-5, trainingCost * 120));
+
+		cout << validationCost << "\n";
+
+		if (validationCost <= 1e-4) {
+			return false; // stop training
 		}
 
-		if (t < warmup + hold) {
-			return max;
-		}
 
-		return (std::cos((t - warmup - hold) * PI / (span - warmup - hold)) + 1) / 2 * max;
-	}
-};
-
-
-
-struct NN {
-	vector<int> sizes;
-	size_t L;
-
-	vector<Mat> Ws;
-	vector<Vec> Bs;
-
-	vector<Adam<Mat>> optimizersW;
-	vector<Adam<Vec>> optimizersB;
-
-
-	NN(const vector<int>& sizes) : sizes(sizes) {
-		L = sizes.size();
-
-		for (size_t i = 0; i < L - 1; ++i) {
-
-			// He initialization
-			float standardDeviation = std::sqrt(2 / sizes[i]);
-			std::normal_distribution<double> distribution(0.0, standardDeviation);
-
-			// Xavier initialization
-			Mat Wi = Mat::random(sizes[i + 1], sizes[i], 0, standardDeviation);
-			Vec Bi = Vec::zeros(sizes[i + 1]) + 0.01;
-
-			Ws.push_back(Wi);
-			Bs.push_back(Bi);
-
-			// DON'T MAKE POINTERS TO VECTORS IDIOTTTT!!!!!!!!!!!!!!!
-		//	optimizersW.push_back(Adam<Mat>(Ws[i]));
-		//	optimizersB.push_back(Adam<Vec>(Bs[i]));
-		}
-
-		for (size_t i = 0; i < L - 1; ++i) {
-			// only if you are shure the vector is not being modified no more!
-			// the reason to that is due to vectors flexibility in their size
-			// they constantly jump around in memory so if you make a pointer
-			// to a vector element, and later add something to  the vector,
-			// it might change places, making that pointer useless!
-			optimizersW.push_back(Adam<Mat>(Ws[i]));
-			optimizersB.push_back(Adam<Vec>(Bs[i]));
-		}
-	}
-
-
-	Vec feedForward(Vec inp) {
-		for (size_t i = 0; i < L - 1; ++i) {
-			inp = activation(Ws[i] * inp + Bs[i]);
-		}
-
-		return inp;
-	}
-
-
-
-
-
-
-	void accumulateGradient(DataPoint& data, vector<Mat>& gradW, vector<Vec>& gradB) {
-
-		vector<Vec> As = { data.x };
-		vector<Vec> Zs;
-
-		for (size_t i = 0; i < L - 1; ++i) {
-			Zs.push_back((Ws[i] * As[i]) + Bs[i]);
-			As.push_back(activation(Zs[i]));
-		}
-
-	//	Vec cd = costDerivative(As[L - 1], y);
-	//	Vec ad = activationDerivative(Zs[L - 2]);
-
-	//	Vec delta = Vec::hadamard(cd, ad);
-		Vec delta = costDerivative(As[L - 1], data.y);
-		gradB[L - 2] += delta;
-		gradW[L - 2] += Vec::outer(delta, As[L - 2]);
-
-		for (size_t i = 2; i < L; ++i) {
-			Mat transposed = Mat::transpose(Ws[L - i]);
-			Vec ac = activationDerivative(Zs[L - 1 - i]);
-
-			delta = transposed * delta;
-			delta = Vec::hadamard(delta, ac);
-
-			gradB[L - 1 - i] += delta;
-			gradW[L - 1 - i] += Vec::outer(delta, As[L - i - 1]);
-		}
-	}
-
-
-	// uses SGD
-	void fit(Dataset& training, const std::function<bool(int)>& updateFunction = NN::updatePositive, 
-			 int iterationsUntilUpdate = 10000, int batchSize = 5, int maxIterations = 1000000) {
-
-		int dataSeen = 0;
-
-		// create vectors to store the accumulated gradient;
-		vector<Mat> accumulatedGradW;
-		vector<Vec> accumulatedGradB;
-
-		for (size_t i = 0; i < L - 1; ++i) {
-			accumulatedGradW.push_back(Mat::zeros(Ws[i].size));
-			accumulatedGradB.push_back(Vec::zeros(Bs[i].size));
-		}
-
-		while (dataSeen < maxIterations) {
-			training.shuffle();
-
-			for (size_t i = 0; i <= training.size - batchSize; i += batchSize) {
-
-				for (size_t j = 0; j < L - 1; ++j) {
-					accumulatedGradW[j] = Mat::zeros(Ws[j].size);
-					accumulatedGradB[j] = Vec::zeros(Bs[j].size);
-				}
-
-				// accumulate the gradients
-				for (int j = 0; j < batchSize; ++j) {
-					++dataSeen;
-					accumulateGradient(training[i + j], accumulatedGradW, accumulatedGradB);
-				}
-
-				// update the weights and biases with the avarages
-				for (size_t j = 0; j < L - 1; ++j) {
-					optimizersW[j].step(accumulatedGradW[j] / batchSize);
-					optimizersB[j].step(accumulatedGradB[j] / batchSize);
-				}
-
-				if (dataSeen % iterationsUntilUpdate < batchSize) {
-					// update function should return a bool indicating if training should continue
-					if (!updateFunction(dataSeen)) {
-						return;
-					}
-				}
-			}
-		}
-
-		cout << "Too much iterations!" << "\n";
-	}
-
-	static bool updatePositive(int dataSeen) {
 		return true;
-	}
+	};
+
+	network.fit(training, updateFunc);
 
 
-	double estimateCost(Dataset dataset, int step = 1) {
-		double costSum = 0;
-		int samplePoints = 0;
+	// waits untill the user closes the graph
+	graph.waitFinish();
 
-		for (size_t j = 0; j < dataset.size; j += step) {
-			DataPoint data = dataset[j];
-
-			Vec pred = feedForward(data.x);
-			costSum += cost(pred, data.y);
-			++samplePoints;
-		}
-
-		return costSum / samplePoints;
-	}
-
-};
-
-
-
-
-
-#endif
+	return 0;
+}
