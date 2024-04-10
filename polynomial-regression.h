184d7730a1ee36e2d3f5ed7407961e19f0a67a60
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-04-10 10:41:07 -0300

    Update polynomial-regression.h
    starting to make a better polynomial regression

--- polynomial-regression.h
+++ polynomial-regression.h
@@ -10,70 +10,254 @@
 
 using namespace std;
 
-#define PI 3.1415926535
-
-template <typename T>
-struct Adam {
-	double a = 0.005;
-	double b1 = 0.9;
-	double b2 = 0.999;
-	double l = 0.01;
-
-	int t = 0;
-
-	double b1PowerT = 1;
-	double b2PowerT = 1;
-
-	T m;
-	T v;
-	T& x;
 
 
-	Adam(T& parameter) : x(parameter), m(T::zeros(parameter.size)), v(T::zeros(parameter.size)) {
 
+
+
+
+
+
+
+
+struct OrthogonalPolynomials {
+	vector<Vec> polynomials;
+	Vec inners;
+	vector<Vec> evaluations;
+
+	Mat x;
+
+	// current degree of polynomials and dimension of points
+	int n = 0;
+	size_t m;
+
+	OrthogonalPolynomials(const Mat& points) : x(points) {
+		m = x.col;
 	}
 
-	void step(T& gradient) {
-		++t;
-		b1PowerT *= b1;
-		b2PowerT *= b2;
-
-		gradient += x * l;
-
-		m = T::lerp(gradient, m, b1);
-		v = T::lerp(T::hadamard(gradient, gradient), v, b2);
-
-		T m_hat = m / (1 - b1PowerT);
-		T v_hat = v / (1 - b2PowerT);
-
-		T c = (m_hat / (T::sqrt(v_hat) + 1e-8) + x * l);
-		double n = linearWarmup(t);
-
-		x -= c * a * n;
-	}
-
-	double linearWarmup(int t) {
-		if (t < 3e4) { // warm-up
-			return 1 * t * 3e-4;
+	Vec getNextPolynomial() {
+		polynomials.resize(polynomials.size() + 1);
+		for (size_t i = 0; i < polynomials.size(); ++i) {
+			polynomials[i].resize((n + 1) * m);
 		}
 
-		return 1 / (1 + 5e-5 * (t - 3e4));
-	}
-
-	double linearWarmupCosAnnealing(int t, int span, int warmup, int hold = 0, double min = 0, double max = 1) {
-		t %= span;
-
-		if (t < warmup) {
-			return (max - min) / warmup * t + min;
+		evaluations.push_back(Vec::zeros(x.size));
+		for (size_t i = 0; i < x.size; ++i) {
+			evaluations[n][i] = evaluatePolynomial(polynomials[n], x[i]);
 		}
 
-		if (t < warmup + hold) {
-			return max;
+		Vec projectionsSum = Vec::zeros((n + 1) * m);
+		for (int i = 0; i <= n; ++i) {
+			double s = polynomialInnerProduct(evaluations[n], evaluations[i]) / inners[i];
+
+			for (int j = 0; j < (n + 1) * m; ++j) {
+				projectionsSum[j] += polynomials[i][j] * s;
+			}
 		}
 
-		return (std::cos((t - warmup - hold) * PI / (span - warmup - hold)) + 1) / 2 * max;
+		for (int j = 0; j < (n + 1) * m; ++j) {
+			polynomials[n][j] -= projectionsSum[j];
+		}
+
+		for (size_t i = 0; i < x.size; ++i) {
+			evaluations[n][i] = evaluatePolynomial(polynomials[n], x[i]);
+		}
+		inners[n] = polynomialInnerProduct(evaluations[n], evaluations[n]);
+
+		++n;
 	}
 };
+
+
+Mat getOrthogonalPolynomials(int n, const Vec& x) {
+
+	Mat polynomials(n, Vec(n));
+	Vec inners(n);
+	Mat evaluations(n, Vec(x.size()));
+
+	for (int i = 0; i < n; ++i) {
+		polynomials[i][i] = 1; // make a i degree polynomial
+
+		// evaluate our new polynomial in all the x points
+		for (size_t j = 0; j < x.size(); ++j) {
+			evaluations[i][j] = evaluatePolynomial(polynomials[i], x[j]);
+		}
+
+		// accumulate the projections
+		Vec projectionsSum = Vec(n);
+		for (int j = 0; j < i; ++j) {
+			double s = polynomialInnerProduct(evaluations[i], evaluations[j]) / inners[j];
+
+			for (int k = 0; k < n; ++k) {
+				projectionsSum[k] += polynomials[j][k] * s;
+			}
+		}
+
+		// make i-th polynomial orthogonal to the rest
+		for (int k = 0; k < n; ++k) {
+			polynomials[i][k] -= projectionsSum[k];
+		}
+
+		// store everything we will need in the future
+		for (size_t j = 0; j < x.size(); ++j) {
+			evaluations[i][j] = evaluatePolynomial(polynomials[i], x[j]);
+		}
+
+		inners[i] = polynomialInnerProduct(evaluations[i], evaluations[i]);
+	}
+
+	return polynomials;
+}
+
+
+
+
+
+
+
+using Vec = std::vector<double>;
+using Mat = std::vector<Vec>;
+
+std::ostream& operator << (std::ostream& os, const Vec& v) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		os << v[i] << ", ";
+	}
+	os << "\n";
+
+	return os;
+}
+
+std::ostream& operator << (std::ostream& os, const Mat& m) {
+	for (size_t i = 0; i < m.size(); ++i) {
+		os << m[i];
+	}
+
+	return os;
+}
+
+
+// uses Horner's method for evaluating polynomials
+double evaluatePolynomial(const Vec& p, const Vec& x) {
+	size_t n = p.size() - 1; // degree of polynomial
+	size_t m = x.size();
+
+	double res = 0;
+
+	double b = p[n];
+	for (size_t i = n; i > 0; --i) {
+		b = p[i - 1] + b * x;
+	}
+
+	return b;
+}
+
+// without pre-evaluation
+double polynomialInnerProduct(const Vec& p1, const Vec& p2, const Vec& x) {
+	double res = 0;
+
+	for (size_t i = 0; i < x.size(); ++i) {
+		res += evaluatePolynomial(p1, x[i]) * evaluatePolynomial(p2, x[i]);
+	}
+
+	return res;
+}
+
+// with pre-evaluated polynomials
+double polynomialInnerProduct(const Vec& e1, const Vec& e2) {
+	double res = 0;
+
+	for (size_t i = 0; i < e1.size(); ++i) {
+		res += e1[i] * e2[i];
+	}
+
+	return res;
+}
+
+
+
+
+
+
+
+
+
+
+
+Vec leastSquares(const Vec& X, const Vec& Y, const Mat& polynomials) {
+	Vec m(polynomials.size());
+
+	for (size_t i = 0; i < polynomials.size(); ++i) {
+		double a = 0;
+		double b = 0;
+
+		for (size_t k = 0; k < X.size(); ++k) {
+			double g = evaluatePolynomial(polynomials[i], X[k]);
+
+			a += g * g;
+			b += Y[k] * g;
+		}
+
+		m[i] = b / a;
+	}
+
+	return m;
+}
+
+
+struct PolynomialRegression {
+	Vec polynomial;
+
+	void fit(const Vec& X, const Vec& Y, int degree = 1) {
+		Mat polynomials = getOrthogonalPolynomials(degree + 1, X);
+		Vec multipliers = leastSquares(X, Y, polynomials);
+
+		// combine all polynomials with it's weights into one (makes it easier to evaluate them)
+		polynomial.resize(degree + 1, 0);
+		for (size_t i = 0; i < polynomials.size(); ++i) {
+			for (size_t j = 0; j < polynomials[i].size(); ++j) {
+				polynomial[j] += polynomials[i][j] * multipliers[i];
+			}
+		}
+	}
+
+	double predict(double x) {
+		return evaluatePolynomial(polynomial, x);
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
