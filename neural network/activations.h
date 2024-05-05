5b57d2858d200b07b6f216e7e074bd7ab082dfa8
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-05-04 22:06:13 -0300

    Update activations.h
    everything is still a bit messy. The derivative function of each class should return the full jacobian (most of the times only the main diagonal is non-zero), and multiplyJacobianVec is implemented to take advantage of the easier matrix-vector multiplication when only the diagonal is set

--- neural network/activations.h
+++ neural network/activations.h
@@ -14,11 +14,34 @@ struct Activation {
     virtual Vec function(const Vec& x) = 0;
     virtual Vec derivative(const Vec& x) = 0;
 
+	// a function to multiply the jacobian by a vector
+	// this is not a pretty solution, but for most activation functions
+	// only the main diagonal of the jacobian is set, so instead of multiplying 
+	// the full jacobian by the vector (O(n^2)), we could multiply the vector 
+	// with only the diagonal of the jacobian element-wise (O(n))
+    virtual Vec multiplyJacobianVec(const Vec& x, const Vec& vector) = 0;
+
     Vec operator () (const Vec& x) {
     	return function(x);
     }
 };
 
+
+struct Linear : Activation {
+
+	Vec function(const Vec& x) override {
+		return x;
+	}
+
+	Vec derivative(const Vec& x) override {
+		return Vec::zeros(x.size) + 1;
+	}
+
+	Vec multiplyJacobianVec(const Vec& x, const Vec& vector) override {
+		return Vec::hadamard(derivative(x), vector);
+	}
+
+};
 
 struct Sigmoid : Activation {
 
@@ -26,9 +49,13 @@ struct Sigmoid : Activation {
 		return 1 / (1 + Vec::exp(-x));
 	}
 
-	Vec derivative(const Vec& x) {
+	Vec derivative(const Vec& x) override {
 		Vec s = function(x);
 		return Vec::hadamard(s, 1 - s);
+	}
+
+	Vec multiplyJacobianVec(const Vec& x, const Vec& vector) override {
+		return Vec::hadamard(derivative(x), vector);
 	}
 
 };
@@ -41,9 +68,62 @@ struct Tanh : Activation {
 		return (e1 - e2) / (e1 + e2);
 	}
 
-	Vec derivative(const Vec& x) {
+	Vec derivative(const Vec& x) override {
 		Vec e = Vec::exp(x) + Vec::exp(-x);
 		return 4 / Vec::hadamard(e, e);
+	}
+
+	Vec multiplyJacobianVec(const Vec& x, const Vec& vector) override {
+		return Vec::hadamard(derivative(x), vector);
+	}
+
+};
+
+struct ReLU : Activation {
+
+	Vec function(const Vec& x) override {
+		return Vec::max(x, Vec::zeros(x.size));
+	}
+
+	Vec derivative(const Vec& x) override {
+		return x > 0;
+	}
+
+	Vec multiplyJacobianVec(const Vec& x, const Vec& vector) override {
+		return Vec::hadamard(derivative(x), vector);
+	}
+
+};
+
+struct Softmax : Activation {
+
+	Vec function(const Vec& x) override {
+		Vec e = Vec::exp(x);
+		return e / Vec::sum(e);
+	}
+
+	// will need to make it so derivative returns the actuall jacobian for everything else
+	Vec derivative(const Vec& x) override {
+		return x;
+	}
+
+	Vec multiplyJacobianVec(const Vec& x, const Vec& vector) override {
+		size_t n = x.size;
+		Vec d = Vec::zeros(n);
+		Vec v = Vec::zeros(n);
+		Vec s = function(x);
+
+		for (size_t i = 0; i < n; ++i) {
+			v[i] = 1.0;
+
+			for (size_t j = 0; j < n; ++j) {
+				d[i] += (v[j] - s[i]) * s[j] * vector[j];
+			}
+
+			v[i] = 0.0;
+		}
+
+		return d;
 	}
 
 };
